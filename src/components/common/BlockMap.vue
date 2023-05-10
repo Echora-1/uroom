90fd365de41a9a49486191b8b8fdf25106818c3d
Echora <kir.ivanov99@mail.ru>
2023-05-10 18:20:02 +0600

    ref

--- src/components/common/BlockMap.vue
+++ src/components/common/BlockMap.vue
@@ -67,14 +67,103 @@ export default {
 };
 </script>
 
+<!--<template>
+  <div ref="map-container" class="block-map"></div>
+</template>
+
+<script>
+import IconMarker from "@/assets/images/marker.svg";
+
+export default {
+  props: {
+    coordinates: {
+      type: Array,
+      default: () => [55.793804, 37.818741],
+    },
+  },
+
+  data() {
+    return {
+      map: null,
+    };
+  },
+
+  mounted() {
+    const container = this.$refs["map-container"];
+    const coord = [this.coordinates[0], this.coordinates[1]];
+    window.ymaps.ready(init);
+    function init() {
+      const ymaps = window.ymaps;
+
+      const map = new ymaps.Map(
+        container,
+        {
+          center: coord,
+          zoom: 13,
+          controls: [],
+        },
+        {
+          searchControlProvider: "yandex#search",
+        }
+      );
+
+      /*      const customStyle = new window.ymaps.MapType("CustomStyle", [
+        {
+          stylers: [
+            { saturation: -100 }, // Насыщенность цвета (-100 = черно-белый)
+            { lightness: 20 }, // Яркость (0 = полная яркость)
+          ],
+        },
+      ]);
+
+      // Добавление созданного набора стилей в хранилище
+      window.ymaps.mapType.storage.add("customStyle", customStyle);
+
+      // Установка созданного набора стилей как активного
+      map.setType("customStyle");*/
+
+      const myPlacemark = new ymaps.Placemark(
+        map.getCenter(),
+        {
+          hintContent: "Собственный значок метки",
+          balloonContent: "Это красивая метка",
+        },
+        {
+          iconLayout: "default#image",
+          iconImageHref: IconMarker,
+          iconImageSize: [58, 58],
+          iconImageOffset: [-29, -29],
+        }
+      );
+
+      map.geoObjects.add(myPlacemark);
+    }
+  },
+};
+</script>-->
+
 <style lang="scss" scoped>
 .block-map {
   width: 100%;
   height: 300px;
 }
 </style>
+
 <style>
-.yandex-container {
-  backdrop-filter: blur(5px);
+.ymaps-2-1-79-places-pane,
+.ymaps-2-1-80-places-pane,
+.ymaps-2-1-81-places-pane,
+.ymaps-2-1-82-places-pane,
+.ymaps-2-1-83-places-pane {
+  z-index: 3000 !important;
+  pointer-events: none;
+}
+
+.ymaps-2-1-79-events-pane,
+.ymaps-2-1-80-events-pane,
+.ymaps-2-1-81-events-pane,
+.ymaps-2-1-82-events-pane,
+.ymaps-2-1-83-events-pane {
+  backdrop-filter: grayscale(1) !important;
 }
 </style>
